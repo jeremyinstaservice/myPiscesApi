b39593c377decb4c35ff6a053b6bb9634f480d35
jeffrey <jeffrey@lizard.global>
2023-08-15 04:35:51 +0100

    voting

--- routes/votesCount.js
+++ routes/votesCount.js
@@ -16,5 +16,24 @@ router.get("/", async (req, res) => {
 });
 
 // UPDATE VOTE COUNT
+router.post("/voting", async (req, res) => {
+  try {
+    const newVotes = req.body.vote_count;
+    const newDate = new Date();
+
+    const Votes = new VoteModel({
+      voteCount: newVotes,
+    });
+
+    const vote = await Votes.save();
+    const { voteCount } = vote._doc;
+    res.status(200).json({ voteCount });
+  } catch (error) {
+    res.status(400).json({
+      message: "Updating vote failed",
+      error: error.message,
+    });
+  }
+});
 
 module.exports = router;
